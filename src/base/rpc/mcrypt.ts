236a540c859885753cc8b8d0eff4c6d0d7e95ce0
Sociosarbis <136657577@qq.com>
2020-11-29 21:26:04 +0800

    feat: :sparkles: add base64 padding

--- src/base/rpc/mcrypt.ts
+++ src/base/rpc/mcrypt.ts
@@ -4,13 +4,18 @@ const algorithms = 'des-ecb';
 const placeHolderIv = Buffer.alloc(0);
 
 function encrypt(buf: Buffer, key) {
+  const pad = 8 - (buf.length % 8);
+  buf = Buffer.concat([buf, Buffer.from(String.fromCharCode(pad).repeat(pad))]);
   const cipher = crypto.createCipheriv(algorithms, key, placeHolderIv);
-  return Buffer.concat([cipher.update(buf), cipher.final()]);
+  return Buffer.concat([cipher.update(buf), cipher.final()]).toString('base64');
 }
 
-function decrypt(buf: Buffer, key) {
+function decrypt(data: string, key) {
+  const buf = Buffer.from(data, 'base64');
   const decipher = crypto.createDecipheriv(algorithms, key, placeHolderIv);
-  return Buffer.concat([decipher.update(buf), decipher.final()]);
+  const decrypted = Buffer.concat([decipher.update(buf), decipher.final()]);
+  const pad = Number(decrypted[decrypted.length - 1].toString());
+  return decrypted.subarray(0, decrypted.length - pad);
 }
 
 export { encrypt, decrypt };
