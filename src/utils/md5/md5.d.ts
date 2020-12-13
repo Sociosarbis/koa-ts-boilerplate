8398ca382529b6ce8180e685545fe6cedd3d4680
Sociosarbis <136657577@qq.com>
2020-12-13 21:08:41 +0800

    feat: :sparkles: try rust-built wasm md5 module

---
+++ src/utils/md5/md5.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export class JsMd5 {
+  free(): void;
+/**
+* @returns {JsMd5}
+*/
+  static new(): JsMd5;
+/**
+* @param {string} s
+*/
+  update(s: string): void;
+/**
+* @returns {string}
+*/
+  finish(): string;
+}
